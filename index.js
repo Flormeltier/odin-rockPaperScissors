6ced6c59da71559ddf1caae9c420d294b3358863
Florian <florianpaulschoene@gmail.com>
2022-09-09 16:06:16 +0200

    error prove
    Co-authored-by: MarcelHimmelreichNovaCapta <MarcelHimmelreichNovaCapta@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ console.log("Start");
 
 function getComputerChoice() {
   let choices = ["Rock", "Paper", "Scissor"];
-  var choice = choices[Math.floor(Math.random() * choices.length)];
+  if (choices.length <= 0) {
+    return "Error";
+  }
+
+  let index = Math.floor(Math.random() * choices.length);
+  if (index >= choices.length || index < 0) {
+    return choices[0];
+  }
+
+  let choice = choices[index];
   return choice;
 }
+
+let message = getComputerChoice();
+console.log(message);

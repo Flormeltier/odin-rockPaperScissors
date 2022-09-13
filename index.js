1e213b862c4335fa45213bc35d1a9fdd0cd932f8
Florian <florianpaulschoene@gmail.com>
2022-09-13 15:35:43 +0200

    working prototype

--- index.js
+++ index.js
@@ -11,9 +11,37 @@ function getComputerChoice() {
     return choices[0];
   }
 
-  let choice = choices[index];
-  return choice;
+  let computerSelection = choices[index];
+  return computerSelection;
+  console.log(computerSelection);
 }
 
-let message = getComputerChoice();
-console.log(message);
+function getPlayerChoice() {
+  let playerSelection = prompt("Enter Rock, Paper or Scissor!");
+  playerSelection = playerSelection.toLowerCase();
+  return playerSelection;
+}
+
+function playOneRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    alert("Draw!");
+  } else if (playerSelection === "rock" && computerSelection === "Paper") {
+    alert("Paper beats Rock! You lose!");
+  } else if (playerSelection === "rock" && computerSelection === "Scissor") {
+    alert("Rock beats Scissor, you win!");
+  } else if (playerSelection === "paper" && computerSelection === "Rock") {
+    alert("Rock beats Paper, you win!");
+  } else if (playerSelection === "paper" && computerSelection === "Scissor") {
+    alert("Scissor beats Paper, you lose");
+  } else if (playerSelection === "scissor" && computerSelection === "Rock") {
+    alert("Rock beats Scissor, you lose!");
+  } else if (playerSelection === "scissor" && computerSelection === "Paper") {
+    alert("Scissor cuts Paper, you win");
+  } else {
+    alert("Wrong input!");
+  }
+}
+
+playerSelection = getPlayerChoice();
+computerSelection = getComputerChoice();
+playOneRound(playerSelection, computerSelection);
